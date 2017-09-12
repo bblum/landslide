de17f7d6c4a6ca588d2cd7697b542f1858a04304
Ben Blum <bblum@andrew.cmu.edu>
2017-09-12 16:09:42 -0400

    have prepare-workspace run ./configure

--- prepare-workspace.sh
+++ prepare-workspace.sh
@@ -1,3 +1,29 @@
 #!/bin/bash
 
-# TODO: do i need to do anything here?
+function die() {
+	echo -ne '\033[01;31m'
+	echo "$1"
+	echo -ne '\033[00m'
+	exit 1
+}
+
+if [ ! -d "pebsim" -o ! -d "src/bochs-2.6.8/instrument/landslide" ]; then
+	die "$0 must be run from the root of the landslide repository."
+fi
+
+INSTALLDIR="$PWD/install"
+CONFIGURE_CMD="./configure --enable-all-optimizations --enable-idle-hack --enable-cpu-level=6 --enable-smp --enable-x86-64 --enable-gameport --enable-disasm --enable-plugins --with-nogui --without-wx --with-x --with-x11 --with-term --enable-debugger --enable-readline --disable-logging --enable-instrumentation=instrument/landslide --prefix=$INSTALLDIR --exec-prefix=$INSTALLDIR"
+
+mkdir -p install || die "couldnt make install dir"
+cd src/bochs-2.6.8 || die "??"
+
+if [ -f "config.status" ]; then
+	if grep -- "--prefix=$INSTALLDIR" config.status >/dev/null; then
+		if grep -- "--exec-prefix=$INSTALLDIR" config.status >/dev/null; then
+			exit 0
+		fi
+	fi
+fi
+
+$CONFIGURE_CMD || die "failed ./configure"
+cd ../../
