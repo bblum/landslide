aed53f66a5bd59788d0b8a78e9976ce823ccf60f
Ben Blum <bblum@andrew.cmu.edu>
2018-03-13 20:43:07 -0400

    [htm] add mario swap microbench

--- pebsim/p2-basecode/410user/inc/new.hpp
+++ pebsim/p2-basecode/410user/inc/new.hpp
@@ -7,10 +7,16 @@
 
 extern "C" {
 #include <malloc.h>
+#include <assert.h>
 }
 
 void* operator new (size_t size) { return malloc(size); }
 
 void operator delete(void *p, unsigned int) { free(p); }
+
+void* operator new[](unsigned int size) { return malloc(size); }
+void operator delete[](void*p) { free(p); }
+
+void __cxa_throw_bad_array_new_length() { assert(0 && "bad array length"); }
 
 #endif /* __NEW_HPP */
