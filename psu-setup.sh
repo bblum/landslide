ecb5afdc286c21e558a585f9290cd80475e83709
Ben Blum <bblum@andrew.cmu.edu>
2018-12-26 15:59:17 -0500

    relocate the psu gnu0xx patch to the new standard patch directory

--- psu-setup.sh
+++ psu-setup.sh
@@ -30,7 +30,7 @@ fi
 if ! grep -- "--std=gnu++0x" src/bochs-2.6.8/instrument/landslide/Makefile.in >/dev/null; then
 	# PSU cluster machines don't have a gcc new enough for gnu++11
 	# -N: forwards (don't ask if seems reversed); -f: force (don't ask questions)
-	patch -N -f -p1 < gross-patches/psu-gnu0xx.patch || die "couldn't patch landslide for gnu0xx"
+	patch -N -f -p1 < src/patches/psu-gnu0xx.patch || die "couldn't patch landslide for gnu0xx"
 fi
 
 # set paths appropriately and configure bochs
